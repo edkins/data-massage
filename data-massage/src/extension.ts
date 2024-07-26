0baf70ff161bfd2a85a6f02bd5136214604d7a22
Giles Edkins <edkins@gmail.com>
2024-07-26 17:41:52 -0400

    Transform plugin into a webview

--- data-massage/src/extension.ts
+++ data-massage/src/extension.ts
@@ -10,16 +10,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "data-massage" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('data-massage.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from data-massage!');
-	});
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider('data-massage', new DataMassageViewProvider())
+	);
+}
 
-	context.subscriptions.push(disposable);
+class DataMassageViewProvider implements vscode.WebviewViewProvider {
+	resolveWebviewView(webviewView: vscode.WebviewView, context: vscode.WebviewViewResolveContext, token: vscode.CancellationToken): Thenable<void> | void {
+		webviewView.webview.html = `<!DOCTYPE html><html><body>hello</body></html>`;
+	}
+
 }
 
 // This method is called when your extension is deactivated
